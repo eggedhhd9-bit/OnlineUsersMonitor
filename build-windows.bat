bf7af15271cb150c1b0695c4a3f02681cb964ddd
eggedhhd9-bit <eggedhhd9@gmail.com>
2026-09-23 01:09:42 -0700

    Build DNS Clean Finder executable

--- build-windows.bat
+++ build-windows.bat
@@ -2,7 +2,7 @@
 setlocal
 py -3 -m pip install --upgrade pip
 py -3 -m pip install -r requirements-build.txt
-py -3 -m PyInstaller --noconfirm --clean --onefile --windowed --name OnlineUsersMonitor online_users_monitor.py
+py -3 -m PyInstaller --noconfirm --clean --onefile --windowed --name DNSCleanFinder dns_clean_finder.py
 if errorlevel 1 exit /b 1
-echo Build complete: dist\OnlineUsersMonitor.exe
+echo Build complete: dist\DNSCleanFinder.exe
 endlocal
