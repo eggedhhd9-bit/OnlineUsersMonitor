4801c6f82372caafa7102f47c0f12e6134d79385
eggedhhd9-bit <eggedhhd9@gmail.com>
2026-09-23 01:23:07 -0700

    Build staff attendance executable

--- build-windows.bat
+++ build-windows.bat
@@ -2,7 +2,7 @@
 setlocal
 py -3 -m pip install --upgrade pip
 py -3 -m pip install -r requirements-build.txt
-py -3 -m PyInstaller --noconfirm --clean --onefile --windowed --name DNSCleanFinder dns_clean_finder.py
+py -3 -m PyInstaller --noconfirm --clean --onefile --windowed --name StaffAttendance staff_attendance.py
 if errorlevel 1 exit /b 1
-echo Build complete: dist\DNSCleanFinder.exe
+echo Build complete: dist\StaffAttendance.exe
 endlocal
